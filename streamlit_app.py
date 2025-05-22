b372ce3752d6eb6f39887b384dbb3b70a9456583
Mito-310 <mitocowri@gmail.com>
2025-05-22 08:07:15 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,4 @@
 import streamlit as st
 
-st.title('To Doリスト：')
+st.title('クイズ：')
 
-agree=st.checkbox('朝６時に起きる')
-if agree:
-    st.write('えらい！一日がんばろう！')
-
-agree=st.checkbox('弁当を作る')
-if agree:
-    st.write('保冷剤入れた？')
