4058c417fb50dd4bafad79a66e4f6c91dcdc1e13
Mito-310 <mitocowri@gmail.com>
2025-05-27 07:39:50 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,2 @@
 import streamlit as st
 
-st.title('クイズ！')
-
-st.write('日田高１年の学年カラーは？')
-
-if st.button('青'):
-    st.write('正解！')
-if st.button('赤'):
-    st.write('残念...')
-if st.button('緑'):
-    st.write('残念...')
