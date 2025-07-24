bf2fd3f0c36868ba22d280a25f2a758290a66ad1
Mito-310 <mitocowri@gmail.com>
2025-07-24 05:38:28 +0000

    wa

--- streamlit_app.py
+++ streamlit_app.py
@@ -603,7 +603,6 @@ st.markdown("""
         transform: translate(-50%, -50%);
         background: linear-gradient(#4CAF50, #45a049);
         color: white;
-        padding: 25px;
         border-radius: 15px;
         font-size: 20px;
         font-weight: bold;
