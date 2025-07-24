dcb8f883ef6a1fdf49d910dc0f6b92704cac6431
Mito-310 <mitocowri@gmail.com>
2025-07-24 04:47:09 +0000

    wa

--- streamlit_app.py
+++ streamlit_app.py
@@ -743,8 +743,7 @@ ctx.strokeStyle = '#333';
                 ctx.moveTo(points[0].x, points[0].y);
                 
                 for (let i = 1; i < points.length; i++) {
-                    ctx.lineTo(points[i].x, points[i].y);
-                }
+                    ctx.lineTo(points[i].x, points[i].y) }
                 
                 ctx.stroke();
                 
