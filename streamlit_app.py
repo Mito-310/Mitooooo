40f7b2a712c8dfd32e9c5c33e89374ba0341581a
Mito-310 <mitocowri@gmail.com>
2025-09-03 12:07:21 +0000

    修正

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import streamlit.components.v1 as components
 
 # ページ設定
 st.set_page_config(
-    page_title="RINGLISH",
+    page_title="Word Connect Game",
     layout="centered",
     initial_sidebar_state="auto"
 )
